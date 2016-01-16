ec7ee45b1dedf69954bc2c794da59d809c7227e9
Devan Kestel <devan.kestel@gmail.com>
2016-01-16 13:07:45 -0500

    Serialized awesomely.

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -2,11 +2,14 @@ class BillsController < ApplicationController
   def index
     api_key = "c32aa663-a302-46b8-b236-e46cc32d160d"
     response = JSON.parse(Bill.get_bills_list(api_key), :symbolize_names => true)
-    response_hash = response[:data].each do |r|
-      r[:attributes]
+    response[:data].each do |bill|
+      puts bill[:attributes][:title]
+      puts bill[:attributes][:identifier]
+      puts bill[:attributes][:legislative_session][:identifier]
+      puts bill[:attributes][:legislative_session][:name]
     end
     @bills = Bill.all
-    binding.pry
+    #binding.pry
     render json: @bills
   end
 end
