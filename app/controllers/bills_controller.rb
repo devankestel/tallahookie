ef7e371f236f5d8f7f5ea28fad1a7b68ebbf3121
Devan Kestel <devan.kestel@gmail.com>
2016-01-16 13:24:10 -0500

    Added :id and :subject

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -3,8 +3,10 @@ class BillsController < ApplicationController
     api_key = "c32aa663-a302-46b8-b236-e46cc32d160d"
     response = JSON.parse(Bill.get_bills_list(api_key), :symbolize_names => true)
     response[:data].each do |bill|
+      puts bill[:id]
       puts bill[:attributes][:title]
       puts bill[:attributes][:identifier]
+      puts bill[:attributes][:subject]
       puts bill[:attributes][:legislative_session][:identifier]
       puts bill[:attributes][:legislative_session][:name]
     end
