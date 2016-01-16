8d17259f73a56e68246e93e6a8f855d0a544e630
Devan Kestel <devan.kestel@gmail.com>
2016-01-16 12:49:22 -0500

    Added in JSON Parsing.

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -1,6 +1,12 @@
 class BillsController < ApplicationController
   def index
+    api_key = "c32aa663-a302-46b8-b236-e46cc32d160d"
+    response = JSON.parse(Bill.get_bills_list(api_key), :symbolize_names => true)
+    response_hash = response[:data].each do |r|
+      r[:attributes]
+    end
     @bills = Bill.all
+    binding.pry
     render json: @bills
   end
 end
--- app/models/bill.rb
+++ app/models/bill.rb
@@ -1,2 +1,7 @@
 class Bill < ActiveRecord::Base
+  private
+  def self.get_bills_list(api_key)
+    base_uri = "https://tabsontallahassee.com/api/bills/"
+    response = HTTParty.get("#{base_uri}?apikey=#{api_key}&legislative_session=2016")
+  end
 end
